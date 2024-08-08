9eca218d66ef678732ac8c0e5567d91c42dc787f
sudhiraslaliya <sudhiraslaliya77@gmail.com>
2024-08-08 18:44:33 -0400

    name changed

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (!$productsResult) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Store</title>
+    <title>Your Furniture Store</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
